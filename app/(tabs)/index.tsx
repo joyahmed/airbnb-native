9a9e5e2dc352e65d3a6c2d66e5eea8b2171d6542
joyahmed <joy_ahmed_007@yahoo.com>
2024-03-23 02:44:12 +0600

    ✅Modals+StackScreens

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Link } from 'expo-router';
+import React from 'react';
+import { View } from 'react-native';
 
 const Page = () => {
-  return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
-}
+	return (
+		<View>
+			<Link href={'/(modals)/login'}>Login</Link>
+			<Link href={'/(modals)/booking'}>Bookings</Link>
+			<Link href={'/listing'}>Listing Details</Link>
+		</View>
+	);
+};
 
-export default Page
+export default Page;
