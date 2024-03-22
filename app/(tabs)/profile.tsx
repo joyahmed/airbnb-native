d4048ba6a647fbd2c2c8a94175026d38a2285734
joyahmed <joy_ahmed_007@yahoo.com>
2024-03-23 02:17:42 +0600

    ✅Tab Screens+Fonts+Colors

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Page = () => {
+	return (
+		<View>
+			<Text>profile</Text>
+		</View>
+	);
+};
+
+export default Page;
